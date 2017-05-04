307afd194a6a5ecbaaf52e77681580383744146b
Reynier Ramos P <reynier.ramos.p@gmail.com>
2017-05-04 12:10:01 +0200

    Web deployment

---
+++ Warehouse/Controllers/Web/FrequencyController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Warehouse.Models;
+
+namespace Warehouse.Controllers.Web
+{
+
+    public class FrequencyController : Controller
+    {
+        private IFrequencyRepository _repository;
+
+        public FrequencyController(IFrequencyRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            try
+            {
+                var data = _repository.GetAll().ToList();
+                return View(data);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        public IActionResult Frequency(string id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+            var data = _repository.Find(Convert.ToInt32(id));
+            return View(data);
+        }
+    }
+}
